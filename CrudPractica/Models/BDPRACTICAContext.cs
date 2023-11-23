378706d0806d9dda2204912ff362833fd432f96c
Josue Bryan Hernandez Zelaya <124007402+JBryan23@users.noreply.github.com>
2023-11-23 10:10:11 -0600

    Initial commit

---
+++ CrudPractica/Models/BDPRACTICAContext.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CrudPractica.Models
+{
+    public partial class BDPRACTICAContext : DbContext
+    {
+        public BDPRACTICAContext()
+        {
+        }
+
+        public BDPRACTICAContext(DbContextOptions<BDPRACTICAContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Estudiante> Estudiantes { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//                optionsBuilder.UseSqlServer("Server=LAPTOP-1I3C54GG\\SQLEXPRESS;Database=BDPRACTICA;integrated security=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Estudiante>(entity =>
+            {
+                entity.Property(e => e.Apellido)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Contraseña)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Fecha).HasColumnType("date");
+
+                entity.Property(e => e.Nombre)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Sexo)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ CrudPractica/Models/Estudiante.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CrudPractica.Models
+{
+    public partial class Estudiante
+    {
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public string? Apellido { get; set; }
+        public int? Edad { get; set; }
+        public string? Sexo { get; set; }
+        public DateTime? Fecha { get; set; }
+        public string? Contraseña { get; set; }
+    }
+}
